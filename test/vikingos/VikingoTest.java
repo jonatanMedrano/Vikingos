1da175a9126e78d3ecb33fd847abbe7d5717c3be
Jonatan Medrano <jonatan.medrano@gmail.com>
2017-09-24 13:17:56 -0300

    Nuevos Tests

--- test/vikingos/VikingoTest.java
+++ test/vikingos/VikingoTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import vikingo.Ebrio;
+import vikingo.EstadoVikingo;
 import vikingo.Vikingo;
 
 public class VikingoTest {
@@ -15,21 +16,31 @@ public class VikingoTest {
 		Vikingo ragnar = new Vikingo();
 		
 		rollo.atacar(ragnar);
-		
-		System.out.println(rollo.getEstado().getAtaque());
-		System.out.println(rollo.getEstado().getDefensa());
-		System.out.println(rollo.getSalud());
-		System.out.println(ragnar.getSalud());
-		
+				
 		Assert.assertEquals(95, ragnar.getSalud());
-		Assert.assertEquals(100, rollo.getSalud());
+		Assert.assertEquals(100, rollo.getSalud());	
+	}
+	
+	@Test
+	public void verificoSacudirAVikingoNormal(){
 		
+		Vikingo rollo = new Vikingo();
+		Vikingo ragnar = new Vikingo();
+		
+		rollo.atacar(ragnar);
+	
 		ragnar.sacudir();
 		
 		ragnar.atacar(rollo);
-		System.out.println(rollo.getSalud());
-		
-		
+		Assert.assertEquals(75, rollo.getSalud());			
 	}
 
+	@Test
+	public void verificoQueSePongaEbrio(){
+		
+		Vikingo rollo = new Vikingo();
+		EstadoVikingo ebrio = new Ebrio();
+		rollo.beber();
+		Assert.assertEquals(ebrio.getClass(), rollo.getEstado().getClass());			
+	}
 }
